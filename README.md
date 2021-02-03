bd1dd8748808ff6b742704d57aa53958841135f3
seanhelvey <sean.helvey@gmail.com>
2021-02-02 18:58:31 -0800

    starting point

---
+++ README.md
@@ -0,0 +1,2 @@
+# Quiz without loops
+Based on https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_slide_down
---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#q1").click(function () {
+    $("#a1").slideDown("slow");
+  });
+  $("#q2").click(function () {
+    $("#a2").slideDown("slow");
+  });
+  $("#q3").click(function () {
+    $("#a3").slideDown("slow");
+  });
+});

396d893d7f0115a5202d1a6b89484ad000fab790
seanhelvey <sean.helvey@gmail.com>
2021-02-02 19:25:13 -0800

    add solution

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 $(document).ready(function () {
-  $("#q1").click(function () {
-    $("#a1").slideDown("slow");
-  });
-  $("#q2").click(function () {
-    $("#a2").slideDown("slow");
-  });
-  $("#q3").click(function () {
-    $("#a3").slideDown("slow");
+  $("#quiz").click(function (event) {
+    var number = event.target.id.slice(-1);
+    $("#a" + number).slideDown("slow");
   });
 });
